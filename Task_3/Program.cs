540ddef0cd80c6b033c4ee999ff57eaf6b0075ba
Aleksandr <b.a.v.7575@mail.ru>
2022-05-02 01:28:26 +0300

    Является ли число чётным

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая на вход принимает число и выдаёт ,
+// является ли число чётным.
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0 )
+{
+  Console.WriteLine("Введённое число - чётное"); 
+  Console.ReadKey();
+}
+else
+{
+    Console.WriteLine("Введённое число - не чётное");
+    Console.ReadKey();
+}
+
